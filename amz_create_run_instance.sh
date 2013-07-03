ad94856c05187ec470c33ca2b888b334ba38ea1d
chrisnatali <chris.natali@gmail.com>
2013-07-03 19:25:15 -0400

    renamed and generalized script to startup an ec2 instance

--- amz_create_run_instance.sh
+++ amz_create_run_instance.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Sample environment params:
-# AMI_ID=ami-eb22be8f
+# AMI_ID=ami-23d9a94a
 # KEY=modilabs
 # SDA_SZ=128
 # INSTANCE_NAME=my-instance
-# IP=<your_ip_address>
+# SECURITY_GROUP=<security-group>
 
 # TODO:  Modify to check if instance exists (and use start) or is running (do nothing)
-instanceid=`ec2-run-instances $AMI_ID -t t1.micro --key $KEY -g 'quicklaunch-1' -b '/dev/sda1=:'$SDA_SZ | grep INSTANCE | cut -f2`
+instanceid=`ec2-run-instances $AMI_ID -t t1.micro --key $KEY -g $SECURITY_GROUP -b '/dev/sda1=:'$SDA_SZ | grep INSTANCE | cut -f2`
 
 # wait for instance to come up
 echo "waiting for $instanceid to enter running state..."
@@ -17,10 +17,8 @@ while ! ec2-describe-instances $instanceid | grep -q running; do sleep 1; done
 
 echo "$instanceid is running"
 
-echo "assigning name osm"
+echo "assigning name"
 ec2addtag $instanceid --tag Name=$INSTANCE_NAME
 
-echo "assigning ip"
-ipaddress="$IP"
+echo "done"
 
-ec2-associate-address -i "$instanceid" "$ipaddress"
