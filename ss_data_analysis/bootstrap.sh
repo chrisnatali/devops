918e3aa92d5b00476b6d1a147e9c566610d741ed
chrisnatali <chris.natali@gmail.com>
2013-11-01 15:32:27 -0400

    added uconv package install for sharedsolar data system

--- ss_data_analysis/bootstrap.sh
+++ ss_data_analysis/bootstrap.sh
@@ -3,7 +3,7 @@
 # setup requirements for shared solar data analysis
 # ONLY VALID FOR ubuntu 12.04 for now
 
-sudo apt-get -y install git python-all-dev postgresql postgresql-contrib
+sudo apt-get -y install git python-all-dev postgresql postgresql-contrib libicu-dev
 
 ## clone ss_data_analysis repo
 git clone git@github.com:modilabs/shared_solar_data_warehouse.git
