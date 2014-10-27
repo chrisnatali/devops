8269f30910132f5b642f71bfe6e02f46fa56d72a
chrisnatali <chris.natali@gmail.com>
2014-10-27 11:51:12 -0400

    install osm2pgsql from scratch and add meta fields to pgsql style file

--- tiles/bootstrap14.sh
+++ tiles/bootstrap14.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 # setup requirements for tile server
-sudo apt-get -y install git curl tmux postgresql-9.3 postgresql-9.3-postgis-2.1 osm2pgsql python-dev libgeos-dev
+sudo apt-get -y install git curl tmux postgresql-9.3 postgresql-9.3-postgis-2.1 python-dev libgeos-dev
+sudo apt-get -y install autoconf automake libtool make g++ libxml2-dev libgeos-dev
+sudo apt-get -y install libgeos++-dev libpq-dev libbz2-dev libproj-dev protobuf-c-compiler
+sudo apt-get -y install libprotobuf-c0-dev lua5.2 liblua5.2-dev
+
+# For osm data sync we use osmosis and osm2pgsql
+# build osm2pgsql to get the latest since apt pkg version doesn't apply data diffs
+git clone https://github.com/openstreetmap/osm2pgsql.git
+cd osm2pgsql
+./autogen.sh
+./configure && make
+sudo make install
+cd
+
+sudo apt-get install osmosis
 
 # specific to tilestache
 sudo apt-get install -qq gdal-bin memcached python-pip
 sudo apt-get install -qq python-imaging python-memcache python-gdal
 sudo apt-get install -qq python-werkzeug python-psycopg2 python-mapnik gunicorn
 
-# tilestache repo with local config
+# tilestache repo with local config 
 git clone https://github.com/SEL-Columbia/TileStache.git
 # set it up
 cd TileStache
