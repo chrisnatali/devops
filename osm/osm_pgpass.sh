845b01da999e66b0f38c299dc51c78a3429747f5
chrisnatali <chris.natali@gmail.com>
2015-01-14 10:52:05 -0500

    moved osm devops to SEL-Columbia/osm-devops

--- osm/osm_pgpass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# setup database access for osm
-
-# add .pgpass pwd file to eliminate password prompt for osm use
-echo "*:*:*:osm:osm" > /home/osm/.pgpass
-chmod 600 /home/osm/.pgpass
-
-
