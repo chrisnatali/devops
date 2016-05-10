89f9b74c371af44e0bf2d707b78f8d84a73f4bfa
chrisnatali <chris.natali@gmail.com>
2016-05-10 14:46:04 -0400

    adding anaconda install

---
+++ anaconda_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# install anaconda for python2 and python3
+# TODO:  Test if batch mode adds conda path to profile
+#        if so, remove it
+#
+# once installed, to use add conda to the path via:
+# export PATH="/home/username/anaconda[23]/bin:$PATH"
+
+wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh
+bash Anaconda2-4.0.0-Linux-x86_64.sh -b
+
+wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+bash Anaconda2-4.0.0-Linux-x86_64.sh -b
